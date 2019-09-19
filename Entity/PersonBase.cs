594779c3c93cde396f400edde7d8fbda36f7ea8b
d7p4n4 <bercikezs199999999@gmail.com>
2019-09-19 16:42:00 +0200

    1.20190919.2

--- Entity/PersonBase.cs
+++ Entity/PersonBase.cs
@@ -7,22 +7,29 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entity
-{          [GUID("c8f60fd3-91f6-438f-9934-666e8cc6c939")]
+{
+    [GUID("efa3e159-7591-427e-a690-c8eb607f2a7f")]
     public class PersonBase
     {
-        [Key]
-        [GUID("0169d284-0796-4e57-a98f-78f022516494")]
+        [GUID("38923767-b8ee-4e29-a90b-5d76f1f6c447")]
+        public Int32 id { get; set; }
+        [GUID("5e32ce63-c7ec-459e-820f-242f0bb87be6")]
         public String name { get; set; }
-        [GUID("d8791a39-1f6b-4641-99a4-3f976f88b899")]
+        [GUID("f505fb64-4986-4efb-88ff-abd8fd23f6c9")]
         public String address { get; set; }
-        [GUID("b79f5215-14d8-480f-8820-257aa33cbf4a")]
+        [GUID("8e2249fc-d0c6-45b0-8ca9-3bb3c0cf6ca6")]
         public Int32 age { get; set; }
-        [GUID("ac33c921-0ba4-4513-a4c0-b9e7544c9a53")]
+        [GUID("ff10d0f3-23a1-43b1-a623-45d8633ad3c6")]
         public Boolean gender { get; set; }
-        [GUID("9a40dde4-0aa7-4361-b396-439196ae4fbf")]
+        [GUID("d40c9fb2-7d1b-4c03-b8fb-e95696f86c96")]
         public List<String> list { get; set; }
 
         public PersonBase() { }
+
+        public Int32 getId()
+        {
+            return id;
+        }
 
         public String getName()
         {
@@ -49,6 +56,11 @@ namespace Entity
             return list;
         }
 
+
+        public void setId(Int32 newValue)
+        {
+            id = newValue;
+        }
 
         public void setName(String newValue)
         {
